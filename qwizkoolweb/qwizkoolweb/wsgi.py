5be7b0539e815a21137fd6236605bc79b7019983
kgvinod <kgvinod@gmail.com>
2021-01-10 21:49:38 -0800

    changes to get apache/wsgi to work

--- qwizkoolweb/qwizkoolweb/wsgi.py
+++ qwizkoolweb/qwizkoolweb/wsgi.py
@@ -7,10 +7,18 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
+
+# add the django project path into the sys.path
+sys.path.append('/home/vinod/work/qwizkool-django/qwizkoolweb/')
+
+# add the virtualenv site-packages path to the sys.path
+sys.path.append('/home/vinod/work/qwizkool-django/.venv/lib/python3.8/site-packages')
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qwizkoolweb.settings')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'qwizkoolweb.settings'
 
 application = get_wsgi_application()
+
